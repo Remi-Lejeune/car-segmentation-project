347ead67d7fa1c9d3817714613fa9d478281811e
PieroRendina <piero.rendina@mail.polimi.it>
2022-10-31 17:20:54 +0100

    Split the dataset

--- data_utils.py
+++ data_utils.py
@@ -6,6 +6,7 @@ from PIL import Image, ImageOps
 from skimage.io import imread
 from skimage.transform import resize
 from sklearn.preprocessing import LabelEncoder
+from torch.utils.data import Dataset, DataLoader
 import warnings
 with warnings.catch_warnings():
     warnings.filterwarnings("ignore", category=DeprecationWarning)
@@ -20,6 +21,27 @@ with warnings.catch_warnings():
 #     for row, col in enumerate(t):
 #         out[int(row), int(col)] = 1
 #     return out
+
+class ImageDataset(Dataset):
+  """
+  This is a custom dataset class. It can get more complex than this, but simplified so you can understand what's happening here without
+  getting bogged down by the preprocessing
+  """
+  def __init__(self, X, Y):
+    self.X = X
+    self.Y = Y
+    if len(self.X) != len(self.Y):
+      raise Exception("The length of X does not match the length of Y")
+
+  def __len__(self):
+    return len(self.X)
+
+  def __getitem__(self, index):
+    # note that this isn't randomly selecting. It's a simple get a single item that represents an x and y
+    _x = self.X[index]
+    _y = self.Y[index]
+
+    return _x, _y
 
 
 class batch_generator():
