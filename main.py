88573f5293f92c2202a77bf0743c9150cd5cfb1f
remi <remi@lejeunebernot.net>
2022-10-31 15:45:41 +0100

    Add data utils and week 6 code

--- main.py
+++ main.py
@@ -1,5 +1,169 @@
 import numpy as np
+from IPython.core.display_functions import clear_output
+from skimage import io
+from matplotlib import pyplot as plt
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.optim as optim
+from torch.nn import Linear, GRU, Conv2d, Dropout, MaxPool2d, BatchNorm1d
+from torch.nn.functional import relu, elu, relu6, sigmoid, tanh, softmax
+import data_utils
 
-print("tete")
-x = 1
-print("fewfewfew")
+data = np.load('carseg_data/clean_data/0.npy')
+print(data)
+
+plt.imshow(data[3])
+plt.show()
+
+use_cuda = torch.cuda.is_available()
+print("Running GPU.") if use_cuda else print("No GPU available.")
+
+batch_size = 32
+IMAGE_SHAPE = (3, 256, 256)
+NUM_FEATURES = 3* 256 * 256
+
+def get_variable(x):
+    """ Converts tensors to cuda, if available. """
+    if use_cuda:
+        return x.cuda()
+    return x
+
+
+def get_numpy(x):
+    """ Get numpy array for both cuda and not. """
+    if use_cuda:
+        return x.cpu().data.numpy()
+    return x.data.numpy()
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+
+    def forward(self, x_img, x_margin, x_shape, x_texture):
+        features = []
+        out = {}
+
+net = Net()
+if use_cuda:
+    net.cuda()
+print(net)
+
+LEARNING_RATE = 0.002
+criterion = nn.CrossEntropyLoss()          # <-- Your code here.
+
+# weight_decay is equal to L2 regularization
+optimizer = optim.AdamW(net.parameters(), lr=LEARNING_RATE, weight_decay=1e-4)
+
+def accuracy(ys, ts):
+    predictions = torch.max(ys, 1)[1]
+    correct_prediction = torch.eq(predictions, ts)
+    return torch.mean(correct_prediction.float())
+
+_img_shape = tuple([batch_size] + list(IMAGE_SHAPE))
+_feature_shape = (batch_size, NUM_FEATURES)
+
+def randnorm(size):
+    return np.random.normal(0, 1, size).astype('float32')
+
+# dummy data
+_x_image = get_variable(Variable(torch.from_numpy(randnorm(_img_shape))))
+_x_margin = get_variable(Variable(torch.from_numpy(randnorm(_feature_shape))))
+_x_shape = get_variable(Variable(torch.from_numpy(randnorm(_feature_shape))))
+_x_texture = get_variable(Variable(torch.from_numpy(randnorm(_feature_shape))))
+
+# test the forward pass
+output = net(x_img=_x_image, x_margin=_x_margin, x_shape=_x_shape, x_texture=_x_texture)
+output['out']
+
+# Setup settings for training
+VALIDATION_SIZE = 0.1  # 0.1 is ~ 100 samples for validation
+max_iter = 3000
+log_every = 100
+eval_every = 100
+
+
+# Function to get label
+def get_labels(batch):
+    return get_variable(Variable(torch.from_numpy(batch['ts']).long()))
+
+
+# Function to get input
+def get_input(batch):
+    return {
+        'x_img': get_variable(Variable(torch.from_numpy(batch['images']))),
+        'x_margin': get_variable(Variable(torch.from_numpy(batch['margins']))),
+        'x_shape': get_variable(Variable(torch.from_numpy(batch['shapes']))),
+        'x_texture': get_variable(Variable(torch.from_numpy(batch['textures'])))
+    }
+
+
+# Initialize lists for training and validation
+train_iter = []
+train_loss, train_accs = [], []
+valid_iter = []
+valid_loss, valid_accs = [], []
+
+# Generate batches
+batch_gen = data_utils.batch_generator(data,
+                                       batch_size=batch_size,
+                                       num_classes=0,
+                                       num_iterations=max_iter,
+                                       seed=42,
+                                       val_size=VALIDATION_SIZE)
+
+# Train network
+net.train()
+for i, batch_train in enumerate(batch_gen.gen_train()):
+    if i % eval_every == 0:
+
+        # Do the validaiton
+        net.eval()
+        val_losses, val_accs, val_lengths = 0, 0, 0
+        for batch_valid, num in batch_gen.gen_valid():
+            output = net(**get_input(batch_valid))
+            labels_argmax = torch.max(get_labels(batch_valid), 1)[1]
+            val_losses += criterion(output['out'], labels_argmax) * num
+            val_accs += accuracy(output['out'], labels_argmax) * num
+            val_lengths += num
+
+        # Divide by the total accumulated batch sizes
+        val_losses /= val_lengths
+        val_accs /= val_lengths
+        valid_loss.append(get_numpy(val_losses))
+        valid_accs.append(get_numpy(val_accs))
+        valid_iter.append(i)
+        print("Valid, it: {} loss: {:.2f} accs: {:.2f}\n".format(i, valid_loss[-1], valid_accs[-1]))
+        net.train()
+
+    # Train network
+    output = net(**get_input(batch_train))
+    labels_argmax = torch.max(get_labels(batch_train), 1)[1]
+    batch_loss = criterion(output['out'], labels_argmax)
+
+    train_iter.append(i)
+    train_loss.append(float(get_numpy(batch_loss)))
+    train_accs.append(float(get_numpy(accuracy(output['out'], labels_argmax))))
+
+    optimizer.zero_grad()
+    batch_loss.backward()
+    optimizer.step()
+
+    # Log i figure
+    if i % log_every == 0:
+        fig = plt.figure(figsize=(12, 4))
+        plt.subplot(1, 2, 1)
+        plt.plot(train_iter, train_loss, label='train_loss')
+        plt.plot(valid_iter, valid_loss, label='valid_loss')
+        plt.legend()
+
+        plt.subplot(1, 2, 2)
+        plt.plot(train_iter, train_accs, label='train_accs')
+        plt.plot(valid_iter, valid_accs, label='valid_accs')
+        plt.legend()
+        plt.show()
+        clear_output(wait=True)
+        # print("Train, it: {} loss: {:.2f} accs: {:.2f}".format(i, train_loss[-1], train_accs[-1]))
+
+    if max_iter < i:
+        break
---
+++ playground.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+data = np.load('carseg_data/clean_data/0.npy')
+print(data.shape)
