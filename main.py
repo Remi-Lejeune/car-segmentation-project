69e3df8640e4d0fec4e5fbc7fdc0b163955bf9aa
remi <remi@lejeunebernot.net>
2022-11-06 23:41:39 +0100

    Add working batch

--- main.py
+++ main.py
@@ -5,35 +5,44 @@ from IPython.core.display import clear_output
 from skimage import io
 from matplotlib import pyplot as plt
 import torch
+from sklearn.metrics import accuracy_score
 from torch.autograd import Variable
 import torch.nn as nn
 import torch.optim as optim
-from torch.nn import Linear, GRU, Conv2d, Dropout, MaxPool2d, BatchNorm1d
+from torch.nn import Linear, GRU, Conv2d, Dropout, MaxPool2d, BatchNorm1d, Parameter, init
 from torch.nn.functional import relu, elu, relu6, sigmoid, tanh, softmax
 from torch.utils.data import DataLoader
+from torchvision.datasets import MNIST
+import torch.nn.functional as F
 
 import data_utils
 
 data = []
 i = 0
 
+#
+# # normalize the inputs
+# x_train.div_(255)
+# x_valid.div_(255)
+# x_test.div_(255)
+
+
 # Read the images
 for np_name in glob.glob('carseg_data/clean_data/*.np[yz]'):
     data.append(np.load(f'{np_name}'))
 
     i += 1
-    if i == 10:
+    if i == 100:
         break
 data = np.array(data)
 print(data.shape)
 
 np.random.shuffle(data)
 
-train_data = data[:int(0.8*data.shape[0])]
-valid_data = data[int(0.8*data.shape[0]):int(0.9*data.shape[0])]
-test_data = data[int(0.9*data.shape[0]):]
+x_train = data[:int(0.8*data.shape[0])]
+x_valid = data[int(0.8*data.shape[0]):int(0.9*data.shape[0])]
+x_test = data[int(0.9*data.shape[0]):]
 
-print(train_data.shape)
 
 
 # for i in range(100):
@@ -64,27 +73,31 @@ def get_numpy(x):
     return x.data.numpy()
 
 
+#Hyperparameters
+num_classes = 10
+num_l1 = 512
+num_features = 262144
+
+# define network
 class Net(nn.Module):
-    def __init__(self):
+
+    def __init__(self, num_features, num_hidden, num_output):
         super(Net, self).__init__()
-        self.activation_fn = nn.ReLU()
-        self.linear = nn.Linear(256 * 256,256 * 256)
+        # input layer
+        self.linear = nn.Linear(512 * 512, 512 * 512)
+        # define activation function in constructor
+        self.activation = torch.nn.ReLU()
 
-    def forward(self, x_img):
-        out = {}
+    def forward(self, x):
+        x = self.linear(x)
+        x = self.activation(x)
 
-        x_img = x_img.flatten()
+        # x = F.linear(x, self.W_2, self.b_2)
 
-        features_final = self.activation_fn(x_img)
-
-        out['out'] = self.l_out(features_final)
-        return out
+        return x
 
 
-net = Net()
-if use_cuda:
-    net.cuda()
-print(net)
+net = Net(num_features, num_l1, num_classes)
 
 LEARNING_RATE = 0.002
 criterion = nn.CrossEntropyLoss()  # <-- Your code here.
@@ -107,18 +120,18 @@ def randnorm(size):
     return np.random.normal(0, 1, size).astype('float32')
 
 
-# dummy data
-_x_image = get_variable(Variable(torch.from_numpy(randnorm(_img_shape))))
-
-# test the forward pass
-output = net(x_img=_x_image, x_margin=_x_margin, x_shape=_x_shape, x_texture=_x_texture)
-output['out']
-
-# Setup settings for training
-VALIDATION_SIZE = 0.1  # 0.1 is ~ 100 samples for validation
-max_iter = 3000
-log_every = 100
-eval_every = 100
+# # dummy data
+# _x_image = get_variable(Variable(torch.from_numpy(randnorm(_img_shape))))
+#
+# # test the forward pass
+# output = net(x_img=_x_image, x_margin=_x_margin, x_shape=_x_shape, x_texture=_x_texture)
+# output['out']
+#
+# # Setup settings for training
+# VALIDATION_SIZE = 0.1  # 0.1 is ~ 100 samples for validation
+# max_iter = 3000
+# log_every = 100
+# eval_every = 100
 
 
 # Function to get label
@@ -133,75 +146,103 @@ def get_input(batch):
     }
 
 
-# Initialize lists for training and validation
-train_iter = []
-train_loss, train_accs = [], []
-valid_iter = []
-valid_loss, valid_accs = [], []
-
-# Generate batches
-batch = iter(DataLoader(data_utils.ImageDataset(train_data[:,0,:,:], train_data[:,3,:,:]), batch_size=4, shuffle=True))
-x, y = next(batch)
-print('x', x.shape)
-print('y', y.shape)
 
 
-# Train network
-net.train()
-for i, batch_train in enumerate():
-    if i % eval_every == 0:
+# setting hyperparameters and gettings epoch sizes
+batch_size = 32
+num_epochs = 200
+num_samples_train = x_train.shape[0]
+num_batches_train = num_samples_train // batch_size
+num_samples_valid = x_valid.shape[0]
+num_batches_valid = num_samples_valid // batch_size
 
-        # Do the validaiton
-        net.eval()
-        val_losses, val_accs, val_lengths = 0, 0, 0
-        for batch_valid, num in batch_gen.gen_valid():
-            output = net(**get_input(batch_valid))
-            labels_argmax = torch.max(get_labels(batch_valid), 1)[1]
-            val_losses += criterion(output['out'], labels_argmax) * num
-            val_accs += accuracy(output['out'], labels_argmax) * num
-            val_lengths += num
+# setting up lists for handling loss/accuracy
+train_acc, train_loss = [], []
+valid_acc, valid_loss = [], []
+test_acc, test_loss = [], []
+cur_loss = 0
+losses = []
 
-        # Divide by the total accumulated batch sizes
-        val_losses /= val_lengths
-        val_accs /= val_lengths
-        valid_loss.append(get_numpy(val_losses))
-        valid_accs.append(get_numpy(val_accs))
-        valid_iter.append(i)
-        print("Valid, it: {} loss: {:.2f} accs: {:.2f}\n".format(i, valid_loss[-1], valid_accs[-1]))
-        net.train()
+def in_range(i, size):
+    if i * size > num_samples_train:
+        return False
+    if (i + 1) * size > num_samples_train:
+        return False
 
-    # Train network
-    output = net(**get_input(batch_train))
-    labels_argmax = torch.max(get_labels(batch_train), 1)[1]
-    batch_loss = criterion(output['out'], labels_argmax)
+    return True
 
-    train_iter.append(i)
-    train_loss.append(float(get_numpy(batch_loss)))
-    train_accs.append(float(get_numpy(accuracy(output['out'], labels_argmax))))
 
-    optimizer.zero_grad()
-    batch_loss.backward()
-    optimizer.step()
+get_slice = lambda i, size: range(i * size, (i + 1) * size)
 
-    # Log i figure
-    if i % log_every == 0:
-        fig = plt.figure(figsize=(12, 4))
-        plt.subplot(1, 2, 1)
-        plt.plot(train_iter, train_loss, label='train_loss')
-        plt.plot(valid_iter, valid_loss, label='valid_loss')
-        plt.legend()
+for epoch in range(num_epochs):
+    # Forward -> Backprob -> Update params
+    ## Train
+    cur_loss = 0
+    net.train()
+    for i in range(num_batches_train):
 
-        plt.subplot(1, 2, 2)
-        plt.plot(train_iter, train_accs, label='train_accs')
-        plt.plot(valid_iter, valid_accs, label='valid_accs')
-        plt.legend()
-        plt.show()
-        clear_output(wait=True)
-        # print("Train, it: {} loss: {:.2f} accs: {:.2f}".format(i, train_loss[-1], train_accs[-1]))
+        if not in_range(i, batch_size):
+            break
 
-    if max_iter < i:
-        break
+        torch.nn.Dropout()
+        optimizer.zero_grad()
+        slce = get_slice(i, batch_size)
+        output = net(torch.tensor(x_train[slce][0].ravel()))
 
+        # compute gradients given loss
+        target_batch = torch.tensor(x_train[slce][3].ravel())
+        batch_loss = criterion(output, target_batch)
+        batch_loss.backward()
+        optimizer.step()
+
+        cur_loss += batch_loss
+    losses.append(cur_loss / batch_size)
+
+    net.eval()
+    ### Evaluate training
+    train_preds, train_targs = [], []
+    for i in range(num_batches_train):
+
+        if not in_range(i, batch_size):
+            break
+
+        slce = get_slice(i, batch_size)
+        output = net(torch.tensor(x_train[slce][0].ravel()))
+
+        preds = torch.max(output, 1)[1]
+
+        train_targs += list(x_train[slce][3].ravel().numpy())
+        train_preds += list(preds.data.numpy())
+
+    ### Evaluate validation
+    val_preds, val_targs = [], []
+    for i in range(num_batches_valid):
+
+        if not in_range(i, batch_size):
+            break
+
+        slce = get_slice(i, batch_size)
+
+        output = net(torch.tensor(x_valid[0][slce].ravel()))
+        preds = torch.max(output, 1)[1]
+        val_targs += list(x_valid[slce][3].ravel().numpy())
+        val_preds += list(preds.data.numpy())
+
+    train_acc_cur = accuracy_score(train_targs, train_preds)
+    valid_acc_cur = accuracy_score(val_targs, val_preds)
+
+    train_acc.append(train_acc_cur)
+    valid_acc.append(valid_acc_cur)
+
+    if epoch % 10 == 0:
+        print("Epoch %2i : Train Loss %f , Train acc %f, Valid acc %f" % (
+            epoch + 1, losses[-1], train_acc_cur, valid_acc_cur))
+
+epoch = np.arange(len(train_acc))
+plt.figure()
+plt.plot(epoch, train_acc, 'r', epoch, valid_acc, 'b')
+plt.legend(['Train Accucary', 'Validation Accuracy'])
+plt.xlabel('Updates'), plt.ylabel('Acc')
 
 
 
