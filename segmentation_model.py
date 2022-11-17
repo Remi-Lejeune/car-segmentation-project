c72a6dfa8b3397362e41fd4de6e2443877dfad3d
remi <remi@lejeunebernot.net>
2022-11-17 17:11:48 +0100

    Sketchy fix that might not work

--- segmentation_model.py
+++ segmentation_model.py
@@ -8,30 +8,30 @@ class SegmentationModel(pl.LightningModule):
     def __init__(self, network):
         super().__init__()
         self.network = network
-        self.loss = nn.NLLLoss()
+        self.loss = nn.CrossEntropyLoss()
 
     def training_step(self, batch, batch_nb):
         x, y = batch
-        x_hat = self.network(x)
-        loss = self.loss(torch.log(x_hat), y)
+        x_hat = self.network(x).flatten(start_dim=2).float()
+        loss = self.loss((x_hat), y.flatten(start_dim=2).float())
 
         self.log("train_loss", loss)
         return loss
 
     def test_step(self, batch, batch_idx):
         x, y = batch
-        x_hat = self.network(x)
-        loss = self.loss(torch.log(x_hat), y)
+        x_hat = self.network(x).flatten(start_dim=2).float()
+        loss = self.loss((x_hat), y.flatten(start_dim=2).float())
 
         self.log("test_loss", loss)
         return loss
 
     def validation_step(self, batch, batch_idx):
         x, y = batch
-        x_hat = self.network(x)
-        # print("x_hat", x_hat.shape)
-        # print("y", y.shape)
-        loss = self.loss(torch.log(x_hat), y)
+        x_hat = self.network(x).flatten(start_dim=2).float()
+        print("x_hat", x_hat.shape)
+        print("y", y.shape)
+        loss = self.loss((x_hat), y.flatten(start_dim=2).float())
 
         self.log("validation_loss", loss)
         return loss
