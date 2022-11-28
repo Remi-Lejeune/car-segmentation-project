bb1fd48cb4c8e55c3e59b7ef132f9da3445e2aff
remi <remi@lejeunebernot.net>
2022-11-28 19:42:13 +0100

    Add test file

--- test.py
+++ test.py
@@ -2,19 +2,11 @@ from image_dataset import *
 
 files_2 = get_files()
 files_a = get_test_files()
+files_clean = get_clean_files()
+print(files_2)
+print(files_a)
+print(files_clean)
 
-#tensor of dimension 9x1x1 with values of 1
-tensor = torch.ones(9, 1)
-print(tensor)
-weights = torch.tensor([1,2,3,4,5,6,7,8,9])
-
-tensor = torch.mul(weights, weights)
-print(tensor)
-# print(files_2)
-# print(len(files_2))
-# print("\n")
-# print(files_a)
-# print(len(files_a))
-# print("\n")
-# print(get_clean_files())
-# print(len(get_clean_files()))
+print(len(files_2))
+print(len(files_a))
+print(len(files_clean))
